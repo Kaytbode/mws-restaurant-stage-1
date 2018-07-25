f7804728a6358dbb92e762401ecc231b38889c1e
Kayode <bkaoluborode@gmail.com>
2018-07-25 14:21:58 +0100

    docs: Add MAPBOX API key
    This is a requirement to gain access to MAPBOX live location
    data.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoia2F5dGJvZGUiLCJhIjoiY2prMHprZ21mMGJidDN2bXEwZmpqMWlnMyJ9.Nlxatou2UgUfYk-4mjsEOQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
